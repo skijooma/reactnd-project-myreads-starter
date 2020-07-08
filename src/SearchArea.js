0af712b67f7951dbd33251575aced88348f32ed3
Steven Kijooma <skijooma@andrew.cmu.edu>
2020-07-08 04:22:46 +0200

    Created BookCabinet; SearchArea; SearchBar; And SearchBookResults components

---
+++ src/SearchArea.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class SearchArea extends Component {
+
+    render(){
+
+        return (
+
+            <div></div>
+        )
+    }
+}
+
+export default SearchArea;
---
+++ src/SearchBookResults.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class SearchBookResults extends Component {
+
+    render(){
+
+        return (
+
+            <div></div>
+        )
+    }
+}
+
+export default SearchBookResults;
